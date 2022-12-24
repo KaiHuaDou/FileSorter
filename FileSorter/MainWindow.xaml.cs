8cfd59d81aae12e85055d0e2b65febc67d18f0c3
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2022-12-24 17:12:11 +0800

    1.0.0.0

--- FileSorter/MainWindow.xaml.cs
+++ FileSorter/MainWindow.xaml.cs
@@ -1,28 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace FileSorter
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow( )
         {
             InitializeComponent( );
+        }
+
+        private void SelectFolder(object o, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog
+            {
+                Description = "选择要整理的文件夹",
+                ShowNewFolderButton = true
+            };
+            if (dialog.ShowDialog( ) == System.Windows.Forms.DialogResult.OK)
+                PathBox.Text = dialog.SelectedPath;
+        }
+
+        private void SortFile(object o, RoutedEventArgs e)
+        {
+            if (!Directory.Exists(PathBox.Text))
+                return;
+            FileInfo[] files;
+            DirectoryInfo folder = new DirectoryInfo(PathBox.Text);
+            string destPath;
+            try
+            {
+                foreach (string item in KeyList.Items)
+                {
+                    files = folder.GetFiles("*" + item + "*");
+                    if (files.Length == 0)
+                        continue;
+                    destPath = Path.Combine(PathBox.Text, item);
+                    if (!Directory.Exists(destPath))
+                        Directory.CreateDirectory(destPath);
+                    foreach (FileInfo info in files)
+                        info.MoveTo(Path.Combine(destPath, info.Name));
+                }
+                FinishPopup.IsOpen = true;
+            }
+            catch (ArgumentException) { }
+        }
+
+        private void AddKey(object o, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(KeyBox.Text))
+                return;
+            KeyList.Items.Add(KeyBox.Text);
+        }
+
+        private void RemoveKey(object o, RoutedEventArgs e)
+        {
+            int cnt = KeyList.SelectedItems.Count;
+            for (int i = 0; i < cnt; i++)
+                KeyList.Items.Remove(KeyList.SelectedItems[0]);
         }
     }
 }
